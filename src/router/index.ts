2cd25f45f469d5d045e343e8df27704fc2613575
Safianov_AI <Aleksandr.Safianov@gt-m.ru>
2021-02-24 16:32:47 +0300

    test

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,11 +1,11 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
-import Home from '../views/Home.vue'
+import Buttons from '../views/Buttons.vue'
 
 const routes: Array<RouteRecordRaw> = [
   {
     path: '/',
-    name: 'Home',
-    component: Home
+    name: 'Buttons',
+    component: Buttons
   },
   {
     path: '/about',
