11a1f483fe48262f373ca4e6b44dde5fcdc3a6fd
Safianov_AI <Aleksandr.Safianov@gt-m.ru>
2021-02-24 15:50:09 +0300

    buttons kit

---
+++ src/components/UIButton.vue
@@ -0,0 +1,117 @@
+<template>
+  <button
+    :class="{
+      [$style.base]: true,
+      [$style[color]]: true,
+    }"
+  >
+    <slot/>
+  </button>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'UiButton',
+  props: {
+    color: {
+      type: String,
+      default: 'primary',
+      validate: (value: string) => [
+        'primary',
+        'secondary',
+        'success',
+        'danger',
+        'warning',
+        'light',
+        'dark'
+      ].includes(value),
+    }
+  },
+
+  computed: {
+    classes(): { [x: number]: boolean } {
+      const {
+        $style,
+        color,
+      } = this;
+
+      return {
+        [$style[color]]: true,
+      };
+    }
+  },
+});
+</script>
+
+<style module lang="less">
+.base {
+  border-radius: 6px;
+  border: none;
+  font-size: 0.9rem;
+  color: white;
+  padding: 10px 25px;
+  cursor: pointer;
+}
+
+.base:focus {
+  outline: none;
+}
+
+.primary {
+  background-color: #007bff;
+  color: white;
+  &:hover {
+    background-color: #006fe6;
+  }
+}
+
+.secondary {
+  background-color: #6c757d;
+  color: white;
+  &:hover {
+    background-color: #65686b;
+  }
+}
+
+.success {
+  background-color: #28a745;
+  color: white;
+  &:hover {
+    background-color: #259940;
+  }
+}
+
+.danger {
+  background-color: #dc3545;
+  color: white;
+  &:hover {
+    background-color: #ca2b3b;
+  }
+}
+
+.warning {
+  background-color: #ffc107;
+  color: #000000;
+  &:hover {
+    background-color: #e9b007;
+  }
+}
+
+.light {
+  background-color: #f8f9fa;
+  color: #000000;
+  &:hover {
+    background-color: #e8e8e9;
+  }
+}
+
+.dark {
+  background-color: #343a40;
+  color: #ffffff;
+  &:hover {
+    background-color: #282d31;
+  }
+}
+</style>
---
+++ src/views/Buttons.vue
@@ -0,0 +1,92 @@
+
+
+<template>
+  <div :class="$style.flex">
+    <div :class="$style.btn">
+      <UIButton
+        color="primary"
+        @click="click"
+      >
+        Primary
+      </UIButton>
+    </div>
+    <div :class="$style.btn">
+      <UIButton
+        color="secondary"
+      >
+        Secondary
+      </UIButton>
+    </div>
+    <div :class="$style.btn">
+      <UIButton
+        color="success"
+      >
+        Success
+      </UIButton>
+    </div>
+    <div :class="$style.btn">
+      <UIButton
+        color="danger"
+      >
+        Danger
+      </UIButton>
+    </div>
+    <div :class="$style.btn">
+      <UIButton
+        color="warning"
+      >
+        Warning
+      </UIButton>
+    </div>
+    <div :class="$style.btn">
+      <UIButton
+        color="light"
+      >
+        Light
+      </UIButton>
+    </div>
+    <div :class="$style.btn">
+      <UIButton
+        color="dark"
+      >
+        Dark
+      </UIButton>
+    </div>
+  </div>
+</template>
+
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import UIButton from '@/components/UIButton.vue';
+
+export default defineComponent({
+  name: 'Home',
+  components: {
+    UIButton,
+  },
+  methods: {
+    click(): void {
+      debugger
+    }
+  }
+});
+</script>
+
+<style module>
+.container {
+  width: 100%;
+  height: 100vh;
+}
+
+.flex {
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+}
+
+.btn {
+  margin: 0 10px;
+}
+
+</style>
